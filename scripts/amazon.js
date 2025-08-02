d6747ba714310268553b806a6a5e5988b9b3632d
Ross01 <ross01_online@yahoo.com>
2025-08-02 11:21:29 +0100

    made cart button interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -80,7 +80,8 @@ products.forEach((product)=> {
             Added
          </div>
 
-         <button class="add-to-cart-button button-primary js-add-to-cart">
+         <button class="add-to-cart-button button-primary js-add-to-cart"
+         data-product-id="${product.id}">
             Add to Cart
          </button>
       </div>
@@ -92,8 +93,28 @@ console.log(productsHTML);
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-document.querySelector('.js-add-to-cart').forEach((button) => {
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
    button.addEventListener('click', () => {
-      console.log('Added product');
+      //console.log('Added product');
+      const productId = button.dataset.productId;
+
+      let matchingItem;
+      cart.forEach((item) => {
+         if (productId === item.productId) {
+            matchingItem = item;
+         }
+      });
+
+      if (matchingItem) {
+         matchingItem.quantity +=1;
+      } else {
+         cart.push({
+            productId: productId,
+            quantity:1
+         });         
+      }
+
+
+      console.log(cart);
    });
 });
