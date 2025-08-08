995fbe2474d47374a15c2cbc1e1d84d47f5f4e0a
Ross01 <ross01_online@yahoo.com>
2025-08-08 12:30:39 +0200

    added .remove method

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -14,9 +14,17 @@ cart.forEach((cartItem) => {
          matchingProduct = product;
       }
    });
+
+
+
    console.log(matchingProduct);
+
+
+
+
    cartSummaryHTML +=`
-   <div class="cart-item-container">
+   <div class="cart-item-container 
+      js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
          Delivery date: Tuesday, June 21
       </div>
@@ -105,5 +113,10 @@ document.querySelectorAll('.js-delete-link').forEach((link)=> {
       removeFromCart(productId); 
       //console.log('delete');
       console.log(cart);
+
+      const container = document.querySelector(`.js-cart-item-container-${productId}`);
+
+      console.log(container);
+      container.remove();
    });
 });
