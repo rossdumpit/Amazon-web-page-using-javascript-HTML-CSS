3a9131081844e7c9d4b771e9a49487a057fe96b9
Ross01 <ross01_online@yahoo.com>
2025-08-16 00:08:06 +0100

    made delivery options interactive

--- data/cart.js
+++ data/cart.js
@@ -3,17 +3,22 @@ export let cart = JSON.parse(localStorage.getItem('cart'));
 
 if(!cart) {
    cart = [{
-   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-   image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', 
    quantity: 2,
+   deliveryOptionId: '1'
 },
 {
    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-   quantity:1
+   quantity: 1,
+   deliveryOptionId: '2'
 }];
 
 }
 
+
+function saveToStorage() {
+   localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
    let matchingItem;
@@ -28,15 +33,15 @@ export function addToCart(productId) {
    } else {
       cart.push({
          productId: productId,
-         quantity:1
+         quantity: 1,
+         deliveryOptionId: '1'
+
       });         
    }
    saveToStorage();
 }
 
-function saveToStorage() {
-   localStorage.setItem('cart', JSON.stringify(cart));
-}
+
 
 export function removeFromCart(productId) {
    const newCart = [];
@@ -48,5 +53,19 @@ export function removeFromCart(productId) {
    });
 
    cart = newCart;
+   saveToStorage();
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchingItem; 
+
+   cart.forEach((cartItem) => {
+      if (productId === cartItem.productId) {
+         matchingItem = cartItem;
+      }
+   }); 
+
+   matchingItem.deliveryOptionId = deliveryOptionId;
+
    saveToStorage();
 }
