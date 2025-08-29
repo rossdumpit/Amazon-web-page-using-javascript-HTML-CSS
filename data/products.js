ca8e81c00bfd531f18bc6c93c56f350269233955
Ross01 <ross01_online@yahoo.com>
2025-08-30 08:23:29 +1000

    added size chart image, used inheritance and polymorphism to add size chart

--- data/products.js
+++ data/products.js
@@ -36,7 +36,48 @@ class Product {
   getPrice() {
     return `$${formatCurrency(this.priceCents)}`;
   }
+
+  extraInfoHTML() {
+    return '';
+  }
 }
+
+class Clothing extends Product {
+  sizeChartLink;
+
+  constructor(productDetails) {
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+   extraInfoHTML() {
+    //super.extraInfoHTML();
+    return `
+      <a href="${this.sizeChartLink}" target= "_blank">Size chart</a>
+    `;
+   }
+
+}
+
+// const tshirt = new Clothing({
+//     id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+//     image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+//     name: "Adults Plain Cotton T-Shirt - 2 Pack",
+//     rating: {
+//       stars: 4.5,
+//       count: 56
+//     },
+//     priceCents: 799, //     keywords: [
+//       "tshirts",
+//       "apparel",
+//       "mens"
+//     ],
+//     type: "clothing",
+//     sizeChartLink: "images/clothing-size-chart.png"
+//   });
+
+//   console.log(tshirt);
+//   console.log(tshirt.getPrice());
+
 
 // const product1 = new Product({
 //     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -716,6 +757,9 @@ export const products = [
     ]
   }
 ].map((productDetails) => {
+  if (productDetails.type === 'clothing') {
+    return new Clothing(productDetails);
+  }
   return new Product(productDetails);
 
 });
