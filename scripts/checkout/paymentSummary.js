43f84382f53a025660627ef77df79ee9d5c989d4
Ross01 <ross01_online@yahoo.com>
2025-09-02 10:25:00 +1000

    created order using backend

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -1,7 +1,9 @@
 import { cart } from "../../data/cart.js";
 import { getProduct } from "../../data/products.js";
 import { getDeliveryOption } from "../../data/deliveryOptions.js";
-import { formatCurrency } from "../utils/money.js"
+import { formatCurrency } from "../utils/money.js";
+import { addOrder } from "../../data/order.js";
+
 
 
 export function renderPaymentSummary() {
@@ -63,7 +65,8 @@ export function renderPaymentSummary() {
             <div class="payment-summary-money">$${formatCurrency(totalCents)}</div>
          </div>
 
-         <button class="place-order-button button-primary">
+         <button class="place-order-button button-primary
+            js-place-order">
             Place your order
          </button>
       
@@ -76,7 +79,35 @@ export function renderPaymentSummary() {
    
    document.querySelector('.js-payment-summary').innerHTML = paymenySummaryHTML
 
+   document.querySelector('.js-place-order').addEventListener('click', async () => {
 
+      try {
+         const response = await fetch('https://supersimplebackend.dev/orders', {
+            method: 'POST',
+            headers: {
+               'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+               cart: cart
+            })
+         });
+
+         const order = await response.json();
+         //console.log(order);
+         addOrder(order);
+
+
+      } catch (error) {
+         console.log('unexpected error. try again');
+
+      }
+
+
+      window.location.href = 'orders.html';
+
+
+
+   });
 
 }
 
